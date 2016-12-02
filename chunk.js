6e5d910e5832765154b7755d430c68e2320739af
chongg039 <chongg039@gmail.com>
2016-12-02 22:33:38 +0800

    添加课表（未完成）

---
+++ chunk.js
@@ -0,0 +1,34 @@
+var getCookie = require('./mockLandOperation.js');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var request = request.defaults({followRedirect: false});
+
+getCookie(function (err, cookies) {
+	const baseUrl = 'http://eams.uestc.edu.cn/eams/';
+
+	// 下面是所有学年成绩
+	var allGradesOptions = {
+		url: baseUrl + 'home!submenus.action?menu.id=',
+		method: 'GET',
+		headers: {
+			'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+			'Accept-Encoding':'deflate, sdch', // don't use gzip!!! else the content may be messied
+			'Accept-Language':'zh-CN,zh;q=0.8',
+			'Cache-Control':'no-cache',
+			'Connection':'keep-alive',
+			'Pragma':'no-cache',
+			'Cookie':cookies,
+			'Host':'eams.uestc.edu.cn',
+			'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+			'X-Requested-With':'XMLHttpRequest'
+		}
+	};
+
+	request(allGradesOptions, function (err, res, body) {
+		console.log(res);
+		// app.get('/', function (req, res) {
+		// 	res.send(body);
+		// });
+	});
+})
